53ff2081d3ac1eb3ac50c563502cbda8e4baeb64
Dmitry Ryabokon <sangvinion1@gmail.com>
2020-09-26 15:05:03 +0300

    Migration from css to scss.

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -5,7 +5,7 @@ import { MOVIES } from '../models/mosk-movies';
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css'],
+  styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
   movies = MOVIES;
