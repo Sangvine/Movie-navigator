0653daabb48932365965fe1db5a382fe88379cc7
Dmitry Ryabokon <sangvinion1@gmail.com>
2020-09-26 16:45:06 +0300

    Grid system for movies.

--- src/app/models/mosk-movies.ts
+++ src/app/models/mosk-movies.ts
@@ -4,4 +4,6 @@ export const MOVIES: Movie[] = [
   { title: 'Фильм 1', genre: 'Жанр 1', country: 'Страна 1' },
   { title: 'Фильм 2', genre: 'Жанр 2', country: 'Страна 2' },
   { title: 'Фильм 3', genre: 'Жанр 3', country: 'Страна 3' },
+  { title: 'Фильм 4', genre: 'Жанр 2', country: 'Страна 2' },
+  { title: 'Фильм 5', genre: 'Жанр 3', country: 'Страна 3' },
 ];
